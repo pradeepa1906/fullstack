c3e3109c893a518f55c8ed5083810d3870a44cbb
pradeepa <skpradeepa2006@gmail.com>
2025-06-16 15:17:27 +0530

    commit message

---
+++ script.js/java.js
@@ -0,0 +1,51 @@
+var a="10";
+var b=10;
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+
+var num=2;
+var result = num%2===0? "even":"odd"
+console.log(result)
+
+var mark=34;
+var result = (mark>=80)? "o":(mark>=60)? "A+":(mark>=35)? "p":"f"
+console.log(result)
+
+function sum(a,b){
+    console.log(a+b);
+    }
+    sum(10,20)
+    var sum = ()=>{
+        var a=10;
+        var b=50;
+        console.log(a+b)
+      }
+      sum();
+
+      let arr = [10,20,30,40];
+      let arr2 = [...arr,50,60,70]
+      console.log(arr);
+      console.log(arr2);
+      var marks =[91,92,93,94,95];
+      var [ m1,m2,m3,m4,m5] =marks;
+      console.log(m1);
+      console.log(m2);
+      console.log(m3);
+      console.log(m4);
+      console.log(m5);
+      
+      
+      
+      
+      
+      
+      
+
+
+
+
+
+
+     
